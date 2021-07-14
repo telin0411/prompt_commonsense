b422afb9c819b8f9abd368a604f7330426919a12
yxli2123 <69247082+yxli2123@users.noreply.github.com>
2021-07-14 09:55:23 +0800

    Update test.sh

--- Com2Sense/test.sh
+++ Com2Sense/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# ** T5-large || SemEval **
-python3 main.py \
---mode test \
---ckpt ./results_log/semeval/t5_large/bs_16/ep_7_stp_8.74k_acc_87.7016_t5_large.pth \
---model t5-large \
---dataset semeval \
---batch_size 32 \
+#** T5-large || SemEval **
+# python3 main.py \
+#--mode test \
+#--ckpt ./results_log/semeval/t5_large/bs_16/ep_7_stp_8.74k_acc_87.7016_t5_large.pth \
+#--model t5-large \
+#--dataset semeval \
+#--batch_size 32 \
 --gpu 0
 
 # ** RoBerta-large || Com2Sense **
