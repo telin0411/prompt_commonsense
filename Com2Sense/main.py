e5aa204a891f9675782558a588443859dc32142f
yxli2123 <69247082+yxli2123@users.noreply.github.com>
2021-07-16 20:03:45 +0800

    add --prompt and prompt_pos as args

--- Com2Sense/main.py
+++ Com2Sense/main.py
@@ -8,6 +8,7 @@ import pandas as pd
 from time import time
 from model import Transformer
 from dataloader import BaseDataset
+
 try:
     from torch.utils.tensorboard import SummaryWriter
 except:
@@ -24,40 +25,47 @@ def main():
     parser = argparse.ArgumentParser(description='Commonsense Dataset Dev')
 
     # Experiment params
-    parser.add_argument('--mode',           type=str,       help='train or test mode', required=True, choices=['train', 'test'])
-    parser.add_argument('--expt_dir',       type=str,       help='root directory to save model & summaries')
-    parser.add_argument('--expt_name',      type=str,       help='expt_dir/expt_name: organize experiments')
-    parser.add_argument('--run_name',       type=str,       help='expt_dir/expt_name/run_name: organize training runs')
-    parser.add_argument('--test_file', type=str, default = 'test', help = 'The file containing test data to evaluate in test mode.')
+    parser.add_argument('--mode', type=str, help='train or test mode', choices=['train', 'test'])
+    parser.add_argument('--expt_dir', type=str, help='root directory to save model & summaries')
+    parser.add_argument('--expt_name', type=str, help='expt_dir/expt_name: organize experiments')
+    parser.add_argument('--run_name', type=str, help='expt_dir/expt_name/run_name: organize training runs')
+    parser.add_argument('--test_file', type=str, default='test',
+                        help='The file containing test data to evaluate in test mode.')
 
     # Model params
-    parser.add_argument('--model',          type=str,       help='transformer model (e.g. roberta-base)', required=True)
-    parser.add_argument('--num_layers', type = int, help = 'Number of hidden layers in transformers (default number if not provided)', default=-1)
-    parser.add_argument('--seq_len',        type=int,       help='tokenized input sequence length', default=256)
-    parser.add_argument('--num_cls',        type=int,       help='model number of classes', default=2)
-    parser.add_argument('--ckpt',           type=str,       help='path to model checkpoint .pth file')
+    parser.add_argument('--model', type=str, default='roberta-large', help='transformer model (e.g. roberta-base)')
+    parser.add_argument('--num_layers', type=int,
+                        help='Number of hidden layers in transformers (default number if not provided)', default=-1)
+    parser.add_argument('--seq_len', type=int, help='tokenized input sequence length', default=256)
+    parser.add_argument('--num_cls', type=int, help='model number of classes', default=2)
+    parser.add_argument('--ckpt', type=str, help='path to model checkpoint .pth file')
 
     # Data params
-    parser.add_argument('--pred_file',      type=str,       help='address of prediction csv file, for "test" mode', default='results.csv')
-    parser.add_argument('--dataset',        type=str,       help='list of datasets seperated by commas', required=True)
+    parser.add_argument('--pred_file', type=str, help='address of prediction csv file, for "test" mode',
+                        default='results.csv')
+    parser.add_argument('--dataset', type=str, default='com2sense', help='list of datasets seperated by commas')
+    parser.add_argument('--prompt', type=str, default='Is the following answer correct? ', help='the prompt')
+    parser.add_argument('--prompt_pos', type=str, default='head', help='options: {head, tail}')
 
     # Training params
-    parser.add_argument('--lr',             type=float,     help='learning rate', default=1e-5)
-    parser.add_argument('--epochs',         type=int,       help='number of epochs', default=100)
-    parser.add_argument('--batch_size',     type=int,       help='batch size', default=8)
-    parser.add_argument('--acc_step',       type=int,       help='gradient accumulation steps', default=1)
-    parser.add_argument('--log_interval',   type=int,       help='interval size for logging training summaries', default=100)
-    parser.add_argument('--save_interval',  type=int,       help='save model after `n` weight update steps', default=30000)
-    parser.add_argument('--val_size',       type=int,       help='validation set size for evaluating metrics', default=2048)
+    parser.add_argument('--lr', type=float, help='learning rate', default=1e-5)
+    parser.add_argument('--epochs', type=int, help='number of epochs', default=100)
+    parser.add_argument('--batch_size', type=int, help='batch size', default=8)
+    parser.add_argument('--acc_step', type=int, help='gradient accumulation steps', default=1)
+    parser.add_argument('--log_interval', type=int, help='interval size for logging training summaries', default=100)
+    parser.add_argument('--save_interval', type=int, help='save model after `n` weight update steps', default=30000)
+    parser.add_argument('--val_size', type=int, help='validation set size for evaluating metrics', default=2048)
 
     # GPU params
-    parser.add_argument('--gpu_ids',        type=str,       help='GPU IDs (0,1,2,..) seperated by comma', default='0')
-    parser.add_argument('-data_parallel',       help='Whether to use nn.dataparallel (currently available for BERT-based models)', action = 'store_true')
-    parser.add_argument('--use_amp',        type=str2bool,  help='Automatic-Mixed Precision (T/F)', default='T')
-    parser.add_argument('-cpu',       help='use cpu only (for test)', action = 'store_true')
+    parser.add_argument('--gpu_ids', type=str, help='GPU IDs (0,1,2,..) seperated by comma', default='0')
+    parser.add_argument('-data_parallel',
+                        help='Whether to use nn.dataparallel (currently available for BERT-based models)',
+                        action='store_true')
+    parser.add_argument('--use_amp', type=str2bool, help='Automatic-Mixed Precision (T/F)', default='T')
+    parser.add_argument('-cpu', help='use cpu only (for test)', action='store_true')
 
     # Misc params
-    parser.add_argument('--num_workers',    type=int,       help='number of worker threads for Dataloader', default=1)
+    parser.add_argument('--num_workers', type=int, help='number of worker threads for Dataloader', default=1)
 
     # Parse Args
     args = parser.parse_args()
@@ -78,7 +86,7 @@ def main():
     text2text = ('t5' in args.model)
     uniqa = ('unified' in args.model)
 
-    assert not (text2text and (args.use_amp) == 'T'), 'use_amp should be F when using T5-based models.'
+    assert not (text2text and args.use_amp == 'T'), 'use_amp should be F when using T5-based models.'
     # Train params
     n_epochs = args.epochs
     batch_size = args.batch_size
@@ -106,13 +114,16 @@ def main():
         print('Training Log Directory: {}\n'.format(log_dir))
 
         # Dataset & Dataloader
-        dataset = BaseDataset('train', tokenizer=args.model, max_seq_len=args.seq_len, text2text=text2text, uniqa = uniqa)
+        dataset = BaseDataset('train', tokenizer=args.model, max_seq_len=args.seq_len,
+                              text2text=text2text, uniqa=uniqa, prompt=args.prompt, promt_pos=args.prompt_pos)
         train_datasets = dataset.concat(dataset_names)
 
-        dataset = BaseDataset('dev', tokenizer=args.model, max_seq_len=args.seq_len, text2text=text2text, uniqa = uniqa)
+        dataset = BaseDataset('dev', tokenizer=args.model, max_seq_len=args.seq_len,
+                              text2text=text2text, uniqa=uniqa, prompt=args.prompt, promt_pos=args.prompt_pos)
         val_datasets = dataset.concat(dataset_names)
 
-        train_loader = DataLoader(train_datasets, batch_size, shuffle=True, drop_last=True, num_workers=args.num_workers)
+        train_loader = DataLoader(train_datasets, batch_size, shuffle=True, drop_last=True,
+                                  num_workers=args.num_workers)
         val_loader = DataLoader(val_datasets, batch_size, shuffle=True, drop_last=True, num_workers=args.num_workers)
 
         # In multi-dataset setups, also track dataset-specific loaders for validation metrics
@@ -139,19 +150,20 @@ def main():
         print_log(log_msg, log_file)
 
         # Build Model
-        model = Transformer(args.model, args.num_cls, text2text, device_ids,num_layers = args.num_layers)
-        if args.data_parallel and  not args.ckpt:
-             model = nn.DataParallel(model, device_ids = device_ids)
-             device = torch.device(f'cuda:{model.device_ids[0]}')
-       
+        model = Transformer(args.model, args.num_cls, text2text, device_ids, num_layers=args.num_layers)
+        print("======The model is like======\n")
+        print(model)
+        if args.data_parallel and not args.ckpt:
+            model = nn.DataParallel(model, device_ids=device_ids)
+            device = torch.device(f'cuda:{model.device_ids[0]}')
+
         if not model.parallelized:
-             model.to(device)
- 
+            model.to(device)
+
         if type(model) != nn.DataParallel:
             if not model.parallelized:
-               model.to(device)
+                model.to(device)
         model.train()
-
 
         # Loss & Optimizer
         criterion = nn.CrossEntropyLoss()
@@ -172,7 +184,7 @@ def main():
             # Load model & optimizer
             model.load_state_dict(checkpoint['model_state_dict'])
             if args.data_parallel:
-                model = nn.DataParallel(model, device_ids = device_ids)
+                model = nn.DataParallel(model, device_ids=device_ids)
                 device = torch.device(f'cuda:{model.device_ids[0]}')
             model.to(device)
 
@@ -193,17 +205,22 @@ def main():
             for batch in tqdm(train_loader):
                 # Load batch to device
                 batch = {k: v.to(device) for k, v in batch.items()}
-
+                # print(tokenizer.decode(batch))
                 with autocast(args.use_amp):
                     if text2text:
                         # Forward + Loss
                         output = model(batch)
+                        # print(output)
                         loss = output[0]
 
                     else:
                         # Forward Pass
                         label_logits = model(batch)
+                        # print("===============label_logits============")
+                        # print(label_logits)
                         label_gt = batch['label']
+                        # print("================label_gt=============")
+                        # print(label_gt)
 
                         # Compute Loss
                         loss = criterion(label_logits, label_gt)
@@ -223,13 +240,14 @@ def main():
                 if curr_step % args.log_interval == 0:
                     # Validation set accuracy
                     if val_datasets:
-                        val_metrics = compute_eval_metrics(model, val_loader, device, val_used_size, tokenizer,text2text, parallel = args.data_parallel)
+                        val_metrics = compute_eval_metrics(model, val_loader, device, val_used_size, tokenizer,
+                                                           text2text, parallel=args.data_parallel)
 
                         # Reset the mode to training
                         model.train()
 
-                        log_msg = 'Validation Accuracy: {:.2f} %  || Validation Loss: {:.4f}'.format(
-                                val_metrics['accuracy'], val_metrics['loss'])
+                        log_msg = 'Validation Accuracy: {:.2f} %  || Validation Loss: {:.4f} || Confusion Matrix: {}\n'.format(
+                            val_metrics['accuracy'], val_metrics['loss'], val_metrics['confusion_matrix'])
 
                         print_log(log_msg, log_file)
 
@@ -244,7 +262,7 @@ def main():
                     time_elapsed = (time() - start_time) / 3600
 
                     log_msg = 'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f} | time elapsed: {:.2f}h |'.format(
-                            epoch, n_epochs, curr_step, steps_per_epoch, loss.item(), time_elapsed)
+                        epoch, n_epochs, curr_step, steps_per_epoch, loss.item(), time_elapsed)
 
                     print_log(log_msg, log_file)
 
@@ -266,17 +284,19 @@ def main():
             # Validation accuracy on the entire set
             if val_datasets:
                 log_msg = '-------------------------------------------------------------------------\n'
-                val_metrics = compute_eval_metrics(model, val_loader, device, val_size, tokenizer, text2text, parallel = args.data_parallel)
+                val_metrics = compute_eval_metrics(model, val_loader, device, val_size, tokenizer, text2text,
+                                                   parallel=args.data_parallel)
 
                 log_msg += '\nAfter {} epoch:\n'.format(epoch)
-                log_msg += 'Validation Accuracy: {:.2f} %  || Validation Loss: {:.4f}\n'.format(
-                    val_metrics['accuracy'], val_metrics['loss'])
+                log_msg += 'Validation Accuracy: {:.2f} %  || Validation Loss: {:.4f} || Confusion Matrix: {}\n'.format(
+                            val_metrics['accuracy'], val_metrics['loss'], val_metrics['confusion_matrix'])
 
                 # For Multi-Dataset setup:
                 if len(dataset_names) > 1:
                     # compute validation set metrics on each dataset independently
                     for loader in val_dataloaders:
-                        metrics = compute_eval_metrics(model, loader, device, val_size, tokenizer, text2text, parallel = args.data_parallel)
+                        metrics = compute_eval_metrics(model, loader, device, val_size, tokenizer, text2text,
+                                                       parallel=args.data_parallel)
 
                         log_msg += '\n --> {}\n'.format(loader.dataset.get_classname())
                         log_msg += 'Validation Accuracy: {:.2f} %  || Validation Loss: {:.4f}\n'.format(
@@ -286,22 +306,23 @@ def main():
                 if val_metrics["accuracy"] > best_val_acc:
                     best_val_acc = val_metrics["accuracy"]
 
-                    step = '{:.1f}k'.format(curr_step/1000) if curr_step > 1000 else '{}'.format(curr_step)
+                    step = '{:.1f}k'.format(curr_step / 1000) if curr_step > 1000 else '{}'.format(curr_step)
                     filename = 'ep_{}_stp_{}_acc_{:.4f}_{}.pth'.format(
-                        epoch, step, best_val_acc, args.model.replace('-', '_').replace('/','_'))
+                        epoch, step, best_val_acc, args.model.replace('-', '_').replace('/', '_'))
 
                     path = os.path.join(log_dir, filename)
                     if args.data_parallel:
-                      model_state_dict = model.module.state_dict()
+                        model_state_dict = model.module.state_dict()
                     else:
-                      model_state_dict = model.state_dict()
+                        model_state_dict = model.state_dict()
                     state_dict = {'model_state_dict': model_state_dict,
                                   'curr_step': curr_step, 'loss': loss.item(),
                                   'epoch': epoch, 'val_accuracy': best_val_acc}
 
                     torch.save(state_dict, path)
 
-                    log_msg += "\n** Best Performing Model: {:.2f} ** \nSaving weights at {}\n".format(best_val_acc, path)
+                    log_msg += "\n** Best Performing Model: {:.2f} ** \nSaving weights at {}\n".format(best_val_acc,
+                                                                                                       path)
 
                 log_msg += '-------------------------------------------------------------------------\n\n'
                 print_log(log_msg, log_file)
@@ -315,14 +336,15 @@ def main():
     elif args.mode == 'test':
 
         # Dataloader
-        dataset = BaseDataset(args.test_file, tokenizer=args.model, max_seq_len=args.seq_len, text2text=text2text, uniqa = uniqa)
+        dataset = BaseDataset(args.test_file, tokenizer=args.model, max_seq_len=args.seq_len, text2text=text2text,
+                              uniqa=uniqa)
         datasets = dataset.concat(dataset_names)
 
         loader = DataLoader(datasets, batch_size, num_workers=args.num_workers)
 
         tokenizer = dataset.get_tokenizer()
 
-        model = Transformer(args.model, args.num_cls, text2text, num_layers = args.num_layers)
+        model = Transformer(args.model, args.num_cls, text2text, num_layers=args.num_layers)
         model.eval()
         model.to(device)
 
@@ -336,7 +358,8 @@ def main():
         is_pairwise = 'com2sense' in dataset_names
 
         # Inference
-        metrics = compute_eval_metrics(model, loader, device, data_len, tokenizer,text2text, is_pairwise =is_pairwise, is_test=True, parallel = args.data_parallel)
+        metrics = compute_eval_metrics(model, loader, device, data_len, tokenizer, text2text, is_pairwise=is_pairwise,
+                                       is_test=True, parallel=args.data_parallel)
 
         df = pd.DataFrame(metrics['meta'])
         df.to_csv(args.pred_file)
