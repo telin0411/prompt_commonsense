8fac3e4554b96a012d5f125f3561d8ecb5a7094f
Yixiao Li <yxli2123@outlook.com>
2021-09-19 18:17:42 +0800

    set test file, 6th time

--- EntangledQA/main.py
+++ EntangledQA/main.py
@@ -346,7 +346,7 @@ def main():
         writer.close()
         log_file.close()
 
-    elif 'test' in args.mode:
+    elif 'test' in args.mode or 'release' in args.mode:
 
         # Dataloader
         dataset = BaseDataset(args.mode, tokenizer=args.model, max_seq_len=args.seq_len, text2text=text2text,
--- EntangledQA/test.sh
+++ EntangledQA/test.sh
@@ -2,37 +2,37 @@
 
 # ** RoBerta-large || EntangledQA **
 python3 main.py \
---mode test-a \
+--mode released-a \
 --ckpt ./results_log/EntangledQA/roberta_large/0/ep_97_stp_389_acc_70.8333_roberta_large.pth \
 --model roberta-large \
 --dataset EntangledQA \
 --batch_size 32 \
---run_name test_a0 \
+--run_name released-a0 \
 --gpu 0 &
 
 python3 main.py \
---mode test-a \
+--mode released-a \
 --ckpt ./results_log/EntangledQA/roberta_large/1/ep_57_stp_229_acc_65.6250_roberta_large.pth \
 --model roberta-large \
 --dataset EntangledQA \
 --batch_size 32 \
---run_name test_a1 \
+--run_name released-a1 \
 --gpu 1 &
 
 python3 main.py \
---mode test-a \
+--mode released-a \
 --ckpt ./results_log/EntangledQA/roberta_large/2/ep_73_stp_293_acc_62.5000_roberta_large.pth \
 --model roberta-large \
 --dataset EntangledQA \
 --batch_size 32 \
---run_name test_a2 \
+--run_name released-a2 \
 --gpu 2 &
 
 python3 main.py \
---mode test-a \
+--mode released-a \
 --ckpt ./results_log/EntangledQA/roberta_large/3/ep_12_stp_49_acc_61.9792_roberta_large.pth \
 --model roberta-large \
 --dataset EntangledQA \
 --batch_size 32 \
---run_name test_a3 \
+--run_name released-a3 \
 --gpu 3 &
