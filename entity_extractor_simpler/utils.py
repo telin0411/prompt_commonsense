d75577a0c0105a27a790f9c0597bb9238022dbc2
Yixiao Li <yxli2123@outlook.com>
2021-10-15 11:17:05 +0800

    commit

--- entity_extractor_simpler/utils.py
+++ entity_extractor_simpler/utils.py
@@ -155,7 +155,7 @@ def pred_entity(model, dataloader, device, tokenizer):
 
         # TODO: add some heuristic threshold from values to limit indices
 
-        input_decoded += [decode(x) for x in batch['input_ids']]
+        input_decoded += [decode(x) for x in batch['input_ids'].to('cpu')]
         print(indices)
         output_decoded += [decode(x) for x in indices]
         label += [decode(x) for x in batch['label']]
