1c39d886b35078f64bd00e2afa2ad67e2e7cc7c7
Yixiao Li <yxli2123@outlook.com>
2021-10-19 22:17:07 +0800

    multi-label

--- entity_extractor_multilabel/main.py
+++ entity_extractor_multilabel/main.py
@@ -203,6 +203,7 @@ def main():
                         # Forward Pass
                         label_logits = model(batch)
                         label_gt = batch['label_binary']
+                        print(batch)
 
                         # Compute Loss
                         loss = criterion(label_logits, label_gt)
