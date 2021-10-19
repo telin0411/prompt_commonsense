72ff700d9b509caf0466af3e1edb9c93d9b355e6
Yixiao Li <yxli2123@outlook.com>
2021-10-19 21:39:20 +0800

    multi-label

--- entity_extractor_multilabel/utils.py
+++ entity_extractor_multilabel/utils.py
@@ -24,13 +24,14 @@ def pred_entity(model, dataloader, device, tokenizer):
 
         # Forward Pass
         label_logits = model(batch)
+        B, L = label_logits.shape
         pred_mask = torch.zeros(label_logits.shape, dtype=torch.int).to(device)
         pred_mask[label_logits > 0] = 1
         print(pred_mask)
 
         input_decoded_batch = [decode(x) for x in batch['input_ids']]
         input_decoded += input_decoded_batch
-        accuracy = (~ torch.logical_xor(batch['label_binary'], pred_mask)).mean()
+        accuracy = (~ torch.logical_xor(batch['label_binary'], pred_mask)).sum() / (B * L)
         acc.append(accuracy.item())
 
         pred = batch['input_ids'] * pred_mask
