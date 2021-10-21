e496a8f42e7ded6420201c6a8a429d14c09b76eb
Yixiao Li <yxli2123@outlook.com>
2021-10-21 19:09:18 +0800

    fix simpler

--- entity_extractor_simpler/utils.py
+++ entity_extractor_simpler/utils.py
@@ -60,12 +60,10 @@ def pred_entity(model, dataloader, device, tokenizer):
     return metric
 
 def compute_acc(source, target):
-    """
     print("===================source====================")
     print(source)
     print("===================target====================")
     print(target)
-    """
     # source [['b0_word_0',..., 'b0_word_k'],..., ['bn_word_0',..., 'bn_word_k']]
     # target [['b0_word_0',..., 'b0_word_k'],..., ['bn_word_0',..., 'bn_word_k']]
     acc = []
